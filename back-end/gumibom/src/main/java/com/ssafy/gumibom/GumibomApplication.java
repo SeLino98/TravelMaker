9f3faa1bf1758a3beb29705c0d904cdabfb3f024
Serot <serotonins023@gmail.com>
2024-01-19 17:19:05 +0900

    back init

---
+++ back-end/gumibom/src/main/java/com/ssafy/gumibom/GumibomApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.gumibom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumibomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumibomApplication.class, args);
+	}
+
+}
