f71c54a318d1a511bc6decda1da03404020496b5
lino <linos98@naver.com>
2024-01-21 18:24:58 +0900

    design:스타일 벡터 추가

--- travelmaker_android/app/src/main/java/com/gumibom/travelmaker/ui/signup/SignupActivity.kt
+++ travelmaker_android/app/src/main/java/com/gumibom/travelmaker/ui/signup/SignupActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
+//@AndroidEntryPoint
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
