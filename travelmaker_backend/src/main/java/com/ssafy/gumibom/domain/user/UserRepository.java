c4f6b91157708b0ed6879e0b7608b316a1a3d1b3
hyeonjae <howlarang123@naver.com>
2024-01-22 16:35:31 +0900

    Feat: 유저 엔티티, 국가&카테고리 엔티티생성

---
+++ travelmaker_backend/src/main/java/com/ssafy/gumibom/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.gumibom.domain.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
+}
