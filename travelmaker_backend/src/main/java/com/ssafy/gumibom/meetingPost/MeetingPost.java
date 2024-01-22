21a4508a1f35bdd54a3f1dce7dd552778b3801f5
Serot <serotonins023@gmail.com>
2024-01-22 16:11:49 +0900

    Feat: meetingPost entity & etc

---
+++ travelmaker_backend/src/main/java/com/ssafy/gumibom/meetingPost/MeetingPost.java
@@ -0,0 +1,46 @@
+package com.ssafy.gumibom.meetingPost;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ssafy.gumibom.entity.Category;
+import com.ssafy.gumibom.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MeetingPost {
+
+    @Id @GeneratedValue
+    @Column(name = "meeting_post_id")
+    private Long id;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "meeting_post")
+    private List<Category> categories = new ArrayList<>();
+
+    private String title;
+    private String content;
+    private LocalDateTime authDate;
+    private Integer nativeMin;
+    private Integer travelerMin;
+    private Integer memberMax;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Boolean status;
+    private LocalDateTime deadline;
+    private String imgUrl;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "meeting_post")
+    private List<User> appliers = new ArrayList<>();
+
+}
