1567a45e0ead5d61fd46f0cee8dec7132e814b7d
Joongwon Lee <wnddnjs823@ajou.ac.kr>
2024-01-28 23:19:48 +0900

    Feat: Pamphlet 서비스 추상 클래스로 변경, MeetingPamphlet과 PersonalPamphlet 서비스 추가

--- travelmaker_backend/src/main/java/com/ssafy/gumibom/domain/pamphlet/service/PamphletService.java
+++ travelmaker_backend/src/main/java/com/ssafy/gumibom/domain/pamphlet/service/PamphletService.java
@@ -1,14 +1,8 @@
 package com.ssafy.gumibom.domain.pamphlet.service;
 
-
-import com.ssafy.gumibom.domain.pamphlet.repository.PamphletRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
 public abstract class PamphletService {
 
 
