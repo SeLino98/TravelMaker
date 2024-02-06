8e511d138f6133a6d1f06306d0d3e4d1a52a5ff4
최지원 <jwchoi.v1@gmail.com>
2024-02-06 17:58:58 +0900

    [feat] mypage에 lottie 이미지 추가

--- travelmaker_android/app/src/main/java/com/gumibom/travelmaker/ui/main/mypage/MainMyPageFragment.kt
+++ travelmaker_android/app/src/main/java/com/gumibom/travelmaker/ui/main/mypage/MainMyPageFragment.kt
@@ -42,44 +42,44 @@ class MainMyPageFragment:Fragment() {
         // 마이페이지에서 일어나는 주요 기능
 
         // 1. 뒤로가기 버튼 클릭 -> 메인화면으로 다시 이동
+        // 이미 toolbar에서 구현 되어있음.
+
         // 2. 프로필사진 변경 버튼 클릭 -> 프로필사진 변경
         mypageProfilePicEdit()
-        // 3. 닉네임 변경 -> edittext 를 눌러서 고침 (liveData)
-        mypageNicknameEdit()
+
+
         // 4. 이메일 변경 -> edittext 를 눌러서 고침 (liveData)
         mypageEmailEdit()
+
         // 5. 회원정보 수정 버튼 클릭 -> 회원정보 수정할 다이얼로그 뜸
+        // 다이얼로그가 뜨게 하는 작업
+        // 다이얼로그에 et를 수정하여, 업데이트 하게 하는 작업
+        // 다이얼로그에
 
         // 6. 로그아웃 버튼 클릭 -> 로그인 상태에서 로그아웃 됨
         mypageLogout()
         // 7. 회원탈퇴 버튼 클릭 -> '정말 탈퇴하겠습니까?' 모달 나오면서 '탈퇴' 클릭 -> 탈퇴
         mypageWithdrawal()
         // 8. 신뢰도 수준 보여주는 함수 <- 신뢰도 점수 구간에 맞게 drawable에서 img_trust_n 사진을 찾아서 그려줌
-//        mypageTrustLevel()
+        mypageCheckTrustLevel()
     }
 
+    private fun mypageCheckTrustLevel(){
+        val btnCheckTrustLevel = binding.btnMypageCheckMyTrustlevel
+
+
+    }
 
     private fun mypageProfilePicEdit(){
         val btnPicEdit = binding.ivMypageProfileEdit
-    }
 
-    private fun mypageNicknameEdit(){
-        val nicknameContent = binding.etMypageNickname
-//        val btnNicknameEdit
-        // 수정 함수 이므로, 유효성 검사 필요
 
     }
 
-    private fun validateNickname(nickname:String): Boolean{
-        if (nickname.isBlank()){
-            Toast.makeText(context,"닉네임을 작성해주세요", Toast.LENGTH_SHORT).show()
-            return false
-        }
-        return true
-    }
 
     private fun mypageEmailEdit(){
         val emailContent = binding.etMypageEmail
+        val btnEmailEdit = binding.ivMypageEmailEdit
         // 수정 함수 이므로, 유효성 검사 필요
 
     }
