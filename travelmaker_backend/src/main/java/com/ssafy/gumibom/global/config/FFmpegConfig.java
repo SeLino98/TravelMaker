c71f036ffcc11afaa143671374e4570d5fa89ba7
Joongwon Lee <wnddnjs823@ajou.ac.kr>
2024-02-10 18:36:47 +0900

    Update: jar 파일에서 ffmpeg 실행파일에 접근할 수 있는 경로 설정 추가

--- travelmaker_backend/src/main/java/com/ssafy/gumibom/global/config/FFmpegConfig.java
+++ travelmaker_backend/src/main/java/com/ssafy/gumibom/global/config/FFmpegConfig.java
@@ -2,12 +2,16 @@ package com.ssafy.gumibom.global.config;
 
 import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFprobe;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FFmpegConfig {
@@ -20,13 +24,35 @@ public class FFmpegConfig {
 
     @Bean
     public FFmpeg ffMpeg() throws IOException {
-        ClassPathResource classPathResource = new ClassPathResource(ffmpegLocation);
-        return new FFmpeg(classPathResource.getURL().getPath());
+//        ClassPathResource classPathResource = new ClassPathResource(ffmpegLocation);
+//        return new FFmpeg(classPathResource.getURL().getPath());
+
+        InputStream inputStream = new ClassPathResource(ffmpegLocation).getInputStream();
+        File file = File.createTempFile("ffmpeg", ".exe");
+
+        try {
+            FileUtils.copyInputStreamToFile(inputStream, file);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
+
+        return new FFmpeg(file.getPath());
     }
 
     @Bean
     public FFprobe ffProbe() throws IOException {
-        ClassPathResource classPathResource = new ClassPathResource(ffprobeLocation);
-        return new FFprobe(classPathResource.getURL().getPath());
+//        ClassPathResource classPathResource = new ClassPathResource(ffprobeLocation);
+//        return new FFprobe(classPathResource.getURL().getPath());
+
+        InputStream inputStream = new ClassPathResource(ffprobeLocation).getInputStream();
+        File file = File.createTempFile("ffprobe", ".exe");
+
+        try {
+            FileUtils.copyInputStreamToFile(inputStream, file);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
+
+        return new FFprobe(file.getPath());
     }
 }
