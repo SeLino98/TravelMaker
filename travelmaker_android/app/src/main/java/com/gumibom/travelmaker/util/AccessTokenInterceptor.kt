1b69b8f969c2971014455a33a54ce900b61d1046
이우건 <bome00519@gmail.com>
2024-02-16 03:17:42 +0900

    update : interceptor 롤백

--- travelmaker_android/app/src/main/java/com/gumibom/travelmaker/util/AccessTokenInterceptor.kt
+++ travelmaker_android/app/src/main/java/com/gumibom/travelmaker/util/AccessTokenInterceptor.kt
@@ -1,5 +1,6 @@
 package com.gumibom.travelmaker.util
 
+import android.util.Log
 import com.gumibom.travelmaker.data.api.token.TokenService
 import com.gumibom.travelmaker.data.repository.login.LoginRepository
 import com.gumibom.travelmaker.model.JwtToken
@@ -9,9 +10,7 @@ import okhttp3.Response
 import javax.inject.Inject
 
 private const val TAG = "AccessTokenInterceptor_싸피"
-class AccessTokenInterceptor @Inject constructor(
-    private val tokenService : TokenService
-) : Interceptor {
+class AccessTokenInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val token = ApplicationClass.sharedPreferencesUtil.getToken()
 
@@ -28,9 +27,11 @@ class AccessTokenInterceptor @Inject constructor(
 //        if (response.code == 401) {
 //            val newJwtToken = runBlocking {
 //                val refreshToken = ApplicationClass.sharedPreferencesUtil.getRefreshToken()
-//                val loginResponseDTO = loginRepositoryImpl.getNewAccessToken(refreshToken)
+//                val tokenResponse = tokenService.getNewJwtToken(refreshToken)
+//                Log.d(TAG, "intercept: $tokenResponse")
 //
-//                JwtToken(loginResponseDTO.accessToken, loginResponseDTO.refreshToken)
+//                val body = tokenResponse.body()!!
+//                JwtToken(body.accessToken, body.refreshToken)
 //            }
 //
 //            if (newJwtToken.accessToken.isNotEmpty()) {
