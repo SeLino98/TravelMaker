ad381815946de57e0fbabb79c7468a8f2f320442
이우건 <bome519@naver.com>
2024-02-15 16:17:39 +0900

    update : Dialog 배경색 투명하게 설정

--- travelmaker_android/app/src/main/java/com/gumibom/travelmaker/ui/common/DialogLoading.kt
+++ travelmaker_android/app/src/main/java/com/gumibom/travelmaker/ui/common/DialogLoading.kt
@@ -2,6 +2,8 @@ package com.gumibom.travelmaker.ui.common
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import com.gumibom.travelmaker.R
 
@@ -10,5 +12,8 @@ class DialogLoading(context : Context) : Dialog(context) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_lottie_animation)
+
+        // 다이얼로그의 Window 객체를 가져와서 배경을 투명하게 설정
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
 }
